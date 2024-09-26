62dfbd17ea2e2d3b7164666c58c014c3950a70a4
kazuki178 <yahashi.kazuki0521@icloud.com>
2024-09-26 16:49:24 +0900

    url変更

--- myPortfolioSite/my-portfolio/src/MainComponent/ItemList.tsx
+++ myPortfolioSite/my-portfolio/src/MainComponent/ItemList.tsx
@@ -9,7 +9,7 @@ const ItemList = () => {
             <ul className='items'>
                 <li><a href="https://x.com/178_c_m_" target='_blank'rel="noreferrer"><FontAwesomeIcon icon={faXTwitter} /></a></li>
                 <li><a href='https://github.com/kazuki178' target='black'rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a></li>
-                <li><a href='https://www.linkedin.com/in/%E5%92%8C%E6%A8%B9-%E7%9F%A2%E6%A9%8B-98b757301/' target='blank'rel="noreferrer"> <FontAwesomeIcon icon={faLinkedin} /></a></li>
+                <li><a href='https://jp.linkedin.com/in/%E5%92%8C%E6%A8%B9-%E7%9F%A2%E6%A9%8B-98b757301' target='blank'rel="noreferrer"> <FontAwesomeIcon icon={faLinkedin} /></a></li>
                 <li><a href='https://qiita.com/178_c_m' target='blank'rel="noreferrer"><FontAwesomeIcon icon={faQuora} /></a></li>
             </ul>
         </div>
